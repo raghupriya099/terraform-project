d3c5590ff99526a5920c234a09f75c00f72baa64
raghupriya099 <132885042+raghupriya099@users.noreply.github.com>
2023-07-11 23:04:33 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
+  name               = "raghu-terraform-elb"
   availability_zones = ["us-east-1a", "us-east-1b"]
 
   listener {
